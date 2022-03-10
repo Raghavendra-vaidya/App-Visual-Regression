75861844d4475d99c23acd66a4ab33ddea800550
Raghavendra vaidya <rkvaidya58@gmail.com>
2022-03-10 13:02:17 +0530

    chore(appium): add utils and config files, dependencies :zap: (#6)

--- src/main/java/com/visual/utilities/CreateMobileDriver.java
+++ src/main/java/com/visual/utilities/CreateMobileDriver.java
@@ -1,6 +1,51 @@
 package com.visual.utilities;
 
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import sun.security.krb5.internal.crypto.Des;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Map;
+
 public class CreateMobileDriver {
+
+    public static AppiumDriver<MobileElement> driver;
+    static DesiredCapabilities desiredCapabilities;
+    static URL appiumHostURL;
+
+    public static DesiredCapabilities setCaps(Map capabilities){
+        desiredCapabilities = new DesiredCapabilities();
+
+        desiredCapabilities.setCapability("deviceName", capabilities.get("deviceName"));
+        desiredCapabilities.setCapability("uuid",capabilities.get("uuid"));
+        desiredCapabilities.setCapability("automationName", capabilities.get("automationName"));
+        desiredCapabilities.setCapability("platformName", capabilities.get("platformName"));
+        desiredCapabilities.setCapability("platformVersion", capabilities.get("platformVersion"));
+        desiredCapabilities.setCapability("appPackage", capabilities.get("appPackage"));
+        desiredCapabilities.setCapability("appActivity", capabilities.get("appActivity"));
+
+        return desiredCapabilities;
+    }
+
+    public static AppiumDriver launchApp(String appiumHost, DesiredCapabilities caps)  {
+        try {appiumHostURL = new URL(appiumHost);}
+        catch (MalformedURLException e) { e.printStackTrace(); }
+
+        driver = new AppiumDriver<MobileElement>(appiumHostURL, caps);
+        return driver;
+    }
+
+    public static AppiumDriver launchAndroid(String appiumHost, DesiredCapabilities androidCaps){
+       return launchApp(appiumHost,androidCaps);
+    }
+
+    public static AppiumDriver launciOS(String appiumHost, DesiredCapabilities iosCaps){
+        return launchApp(appiumHost,iosCaps);
+    }
+
+
 
 
 
